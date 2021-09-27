c63b77645b9fc61f442b4c148475d9070ccc8103
Andrew Giang <giang184@gmail.com>
2021-09-27 16:58:58 -0700

    add card data and preview

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,16 +1,27 @@
 import {useState} from 'react'
 
 const Dashboard = () => {
-  const [name, setName] = useState('Andrew');
+  const [cards, setCards] = useState([
+    {name: 'chase freedom unlimited', type: 'visa', rewards: {travel: 5, restaurant: 3, drugstore: 3, other: 1.5}, user: 'temp', id: 1},
+    {name: 'chase freedom flex', type: 'visa', rewards: {walmart: 5, paypal: 5, travel: 5, restaurant: 3, drugstore: 3, other: 1}, user: 'temp', id: 2},
+    {name: 'chase sapphire preferred', type: 'visa', rewards: {travel: 5, restaurant: 3, grocery: 3, streaming: 3, other: 1}, user: 'temp', id: 3}
+  ]);
 
-  const handleClick = () => {
-    setName('Gloria')
-  }
   return (  
     <div className="home">
-      <h2>Dashboard</h2>
-      <p>{name}</p>
-      <button onClick = {handleClick}>Click me</button>
+        {cards.map((card) => (
+          <div className="card-preview" key={card.id}>
+            <h2>{card.name}</h2>
+            <h4>TYPE: {card.type}</h4>
+            <h4>REWARDS: </h4>
+            {
+                Object.entries(card.rewards).map(([key, val]) => 
+                    <p key={key}>{key}: {val}%</p>
+                )
+            }
+            
+          </div>
+        ))}
     </div>
   );
 }
