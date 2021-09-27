a25bb22be66e0c3472baf55d20851a35c9eac867
Andrew Giang <giang184@gmail.com>
2021-09-27 16:16:39 -0700

    add navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return ( 
+    <nav className="nabar">
+      <h1>Credit Card Suggestor</h1>
+        <a href="/">Home</a>
+        <a href="/create">Add New Card</a>
+    </nav>
+  );
+}
+export default Navbar;
