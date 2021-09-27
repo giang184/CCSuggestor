62efb74cd350ca04089a572c69cd77459639d128
Andrew Giang <giang184@gmail.com>
2021-09-27 16:27:25 -0700

    add hook

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,16 @@
+import {useState} from 'react'
+
 const Dashboard = () => {
+  const [name, setName] = useState('Andrew');
+
+  const handleClick = () => {
+    setName('Gloria')
+  }
   return (  
     <div className="home">
       <h2>Dashboard</h2>
+      <p>{name}</p>
+      <button onClick = {handleClick}>Click me</button>
     </div>
   );
 }
