430f4d667414e77fb95bdb4936d5eaa71f5f5d02
Andrew Giang <giang184@gmail.com>
2021-09-28 14:02:58 -0700

    change cardlist to use firestore

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,4 +1,8 @@
-const CardList = ({cards, title, handleDelete}) => {
+import React from 'react';
+import useFirestore from '../hooks/useFirestore';
+
+const CardList = ({title, handleDelete}) => {
+  const {cards} = useFirestore('cards');
   return (  
     <div className="card-list">
       <h1>{title}</h1>
@@ -6,12 +10,12 @@ const CardList = ({cards, title, handleDelete}) => {
           <div className="card-preview" key={card.id}>
             <h2>{card.name}</h2>
             <h4>TYPE: {card.type}</h4>
-            <h4>REWARDS: </h4>
+            {/* <h4>REWARDS: </h4>
             {
                 Object.entries(card.rewards).map(([key, val]) => 
                     <p key={key}>{key}: {val}%</p>
                 )
-            }
+            } */}
             <button onClick = {() => handleDelete(card.id)}>Delete Card</button>
           </div>
         ))}
