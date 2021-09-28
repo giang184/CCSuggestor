300f2b795b01b0bad3e34553914a765a5134a84a
Andrew Giang <giang184@gmail.com>
2021-09-27 17:43:21 -0700

    change some styling on navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,8 +2,10 @@ const Navbar = () => {
   return ( 
     <nav className="navbar">
       <h1>Credit Card Suggestor</h1>
+      <div className="links">
         <a href="/">Home</a>
         <a href="/create">Add New Card</a>
+        </div>
     </nav>
   );
 }
