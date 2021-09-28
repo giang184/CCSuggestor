ab4c46dad603a3b3b923567e7ade6f5fef3ed580
Andrew Giang <giang184@gmail.com>
2021-09-28 14:03:22 -0700

    add use storag hook

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,14 @@
+import {useEffect} from 'react';
+import {projectFirestore} from '../firebase/config'
+
+const useStorage = ({name, type}) => {
+
+  useEffect(() => {
+    const collectionRef = projectFirestore.collection('cards');
+    collectionRef.add({name: name, type: type})
+  }, [name, type]);
+  
+  return {name, type};
+}
+
+export default useStorage;
