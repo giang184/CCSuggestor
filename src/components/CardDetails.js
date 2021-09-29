e00441ee71d12107775f8a53165dd7e6b92e72b0
Andrew Giang <giang184@gmail.com>
2021-09-29 15:43:44 -0700

    add card details

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,11 +1,23 @@
 import { useParams } from "react-router";
+import useFirestore from '../hooks/useFirestore';
 
 const CardDetails = () => {
   const {id} = useParams();
+  const {cards} = useFirestore('cards');
+  const card = (cards.length > 0) ? cards.filter(card => card.id === id)[0] : {};
 
   return (  
     <div className="card-details">
-      <h2>Card Details: {id}</h2>
+      <div>
+        <h1>{card.name}</h1>
+        <h4>Card Type: {card.type}</h4>
+        <h4>Rewards: </h4>
+        <ul>
+        {card.categories && Object.entries(card.categories).map(([key, val]) => 
+                      <li key={key}>{key}: {val}%</li>
+                  )}
+        </ul>
+      </div>
     </div>
   );
 }
