df3948cb2cb85299fb443db86a62b28002ce77cb
Andrew Giang <giang184@gmail.com>
2021-09-29 11:58:57 -0700

    update addCard

--- src/components/AddCardDynamic.js
+++ src/components/AddCardDynamic.js
@@ -6,13 +6,28 @@ const AddCardDynamic = () => {
   
   const categories = [
     'Gas',
-    'Dining',
-    'Clothes',
+    'Restaurant',
+    'travel',
+    'Grocery Stores',
+    'Wholesale Clubs',
+    'Streaming Services',
+    'Online Shopping',
+    'Home Improvement',
+    'Department Stores',
+    'Paypal',
+    'Target',
+    'Walmart',
+    'Target',
+    'Amazon',
+    'Drug Stores',
+    'Internet/Phone/Cable',
+    'Best Buy',
+    'All other purchases'
   ]
 
   const [formState, setFormState] = useState({
     name: '',
-    type: '',
+    type: 'visa',
     categories: {
     }
   })
@@ -75,29 +90,36 @@ const AddCardDynamic = () => {
   const onSubmit = (event) => {
     event.preventDefault()
     console.log(formState)
+
+    const collectionRef = projectFirestore.collection('cards');
+    collectionRef.add(formState)
   }
 
   return (
     <div className="app">
       <form onSubmit={onSubmit}>
-        <label>Credit Card Name</label>
-        <input 
-          type="text"
-          value={formState.name}
-          onChange={updateName}
-        />
-
-        <label>Card Type: </label>
-        <select
-          value={formState.type}
-          onChange={updateType}
-        >
-          <option value="visa">Visa</option>
-          <option value="mastercard">Mastercard</option>
-        </select>
+        <div className="form-name">
+          <label>Credit Card Name: </label>
+          <input 
+            type="text"
+            required
+            value={formState.name}
+            onChange={updateName}
+          />
+        </div>
+        <div className="form-type">
+          <label>Card Type: </label>
+          <select
+            value={formState.type}
+            onChange={updateType}
+          >
+            <option value="visa">Visa</option>
+            <option value="mastercard">Mastercard</option>
+          </select>
+        </div>
 
         <fieldset>
-          <legend>Add Cash Back Categories</legend>
+          <legend>Choose Cash Back Categories</legend>
 
           <select 
             value={selectedCategory}
@@ -110,24 +132,26 @@ const AddCardDynamic = () => {
 
           <button onClick={addCategory}>Add</button>
         </fieldset>
-
+        {Object.keys(formState.categories).length >0 &&
         <fieldset>
-          <legend>Set Category's Cash Back</legend>
+          <legend>Set Category's Cash Back Percentage</legend>
 
           {Object.keys(formState.categories).map(category => (
             <div key={category}>
               <label>{category}</label>
               <input 
+                type="number"
                 value={formState.categories[category]} 
                 onChange={(event) => updateValueForCategory(category, event.target.value)}
               />
 
-              <button onClick={(event) => removeCategory(event, category)}>Remove Rewards Category</button>
+              <button onClick={(event) => removeCategory(event, category)}>Remove Category</button>
             </div>
           ))}
         </fieldset>
+        }
 
-        <button type="submit">Submit Me Darnit</button>
+        <button type="submit">Create Card</button>
       </form>
     </div>
   )
