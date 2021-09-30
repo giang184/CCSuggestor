e32a47d5694705bc51c9789b88a452dfbf8885d6
Andrew Giang <giang184@gmail.com>
2021-09-30 14:45:55 -0700

    add card with two component

---
+++ src/components/AddCardTemplates.js
@@ -0,0 +1,36 @@
+
+const AddCardTemplates = () => {
+  return ( 
+    <div className="add-card-templates">
+        <h2>Choose From Default List</h2>
+        <div className="d-flex flex-row">
+          <div className="card-template">
+            <img src='https://thumbor.forbes.com/thumbor/fit-in/600x300/https://www.forbes.com/advisor/wp-content/uploads/2019/04/874a5450-5701-11e9-982f-0172e11a2fba.png' class="card-img-top" alt="Chase-Freedom-Unlimited"/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div className="card-template">
+            <img src='https://thumbor.forbes.com/thumbor/fit-in/600x300/https://www.forbes.com/advisor/wp-content/uploads/2019/04/874a5450-5701-11e9-982f-0172e11a2fba.png' class="card-img-top" alt="Chase-Freedom-Unlimited"/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div className="card-template">
+            <img src='https://thumbor.forbes.com/thumbor/fit-in/600x300/https://www.forbes.com/advisor/wp-content/uploads/2019/04/874a5450-5701-11e9-982f-0172e11a2fba.png' class="card-img-top" alt="Chase-Freedom-Unlimited"/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+        </div> 
+  );
+}
+export default AddCardTemplates;
+
