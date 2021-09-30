c7e872d46ee859dc0b1f82a88a06428c7c8c6230
Andrew Giang <giang184@gmail.com>
2021-09-30 14:45:41 -0700

    add card default template component

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,83 +1,16 @@
-// import { useState } from 'react';
-// import { projectFirestore } from '../firebase/config';
+import React, { useState } from 'react';
+import { projectFirestore } from '../firebase/config';
+import AddCardDynamic from './AddCardDynamic';
+import AddCardTemplates from './AddCardTemplates';
 
-// const AddCard = () => {
-//   const [name,setName] = useState('')
-//   const [type,setType] = useState('')
-//   const [category1,setCategory1] = useState()
-//   const [category2,setCategory2] = useState()
-//   // const [category3,setCategory3] = useState()
-//   const [percent1,setPercent1] = useState()
-//   const [percent2,setPercent2] = useState()
-//   // const [percent3,setPercent3] = useState()
+const AddCard = () => {
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     const collectionRef = projectFirestore.collection('cards');
-//     let obj = {};
-//     if(category1 && percent1){ 
-//       obj= {...obj, [category1]: percent1};
-//     }
-//     if(category2 && percent2){ 
-//       obj= {...obj, [category2]: percent2};
-//     }
-//     console.log(obj);
-//     collectionRef.add({name, type, rewards: obj})
-//   }
+  return (  
+    <React.Fragment>
+      <AddCardTemplates />
+      <AddCardDynamic />
+    </React.Fragment>
+  );
+}
 
-//   return (  
-//     <div className="create">
-//       <h2>Add new card</h2>
-//       <form onSubmit={handleSubmit}>
-//         <label>Card Name: </label>
-//         <input 
-//           type="text"
-//           required
-//           onChange={(e) => setName(e.target.value)}
-//         />
-//         <div />
-//         <label>Card Type: </label>
-//         <select 
-//           value={type}
-//           onChange={(e) => setType(e.target.value)}
-//         >
-//           <option value="visa">Visa</option>
-//           <option value="mastercard">Mastercard</option>
-//         </select>
-//         <div />
-//         <div>Rewards:</div>
-//         <select 
-//           value={category1}
-//           onChange={(e) => setCategory1(e.target.value)}
-//         >
-//           <option value=""></option>
-//           <option value="Gas">Gas</option>
-//           <option value="Restaurant">Restaurant</option>
-//         </select>
-//         <input 
-//           value={percent1}
-//           type="text"
-//           onChange={(e) => setPercent1(e.target.value)}
-//         />
-//       <div />
-//         <select 
-//         value={category2}
-//           onChange={(e) => setCategory2(e.target.value)}
-//         >
-//           <option value=""></option>
-//           <option value="Gas">Gas</option>
-//           <option value="Restaurant">Restaurant</option>
-//         </select>
-//         <input 
-//           value={percent2}
-//           type="text"
-//           onChange={(e) => setPercent2(e.target.value)}
-//         />
-//         <div><button>Add Card</button></div>
-        
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default AddCard;
+export default AddCard;
