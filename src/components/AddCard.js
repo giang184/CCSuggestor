1134b2fd9ad32a60f72214bd549a7709be9c2dc1
Andrew Giang <giang184@gmail.com>
2021-09-30 16:20:40 -0700

    remove usestate

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { projectFirestore } from '../firebase/config';
+import React from 'react';
 import AddCardDynamic from './AddCardDynamic';
 import AddCardTemplates from './AddCardTemplates';
 
