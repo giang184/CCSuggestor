f1f7eff8044def97cc8ae5a146fb6dc388c0b34b
Andrew Giang <giang184@gmail.com>
2021-10-05 15:25:05 -0700

    add history

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,39 +1,42 @@
 import React, {useRef, useState} from 'react'
 import { Form, Button, Card, Alert} from 'react-bootstrap'
 import {useAuth} from '../contexts/AuthContext'
+import { Link, useHistory } from "react-router-dom"
 
-const Signup = () => {
+export default function Signup() {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  const {signup, currentUser} = useAuth()
-  const [error, setError] = useState('')
+  const { signup } = useAuth()
+  const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
+  const history = useHistory()
 
   async function handleSubmit(e) {
     e.preventDefault()
-    
-    if(passwordRef !== passwordConfirmRef) {
-      return setError('Passwords do not match')
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match")
     }
-    
+
     try {
-      setError('')
+      setError("")
       setLoading(true)
       await signup(emailRef.current.value, passwordRef.current.value)
+      history.push("/")
     } catch {
-      setError('Failed to create an account')
+      setError("Failed to create an account")
     }
+
     setLoading(false)
   }
-  return (  
+  return (
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4"> Sign up</h2>
-          {currentUser && currentUser.email}
-          {error && <Alert variant='danger'>{error}</Alert>}
-          <Form onSubmit = {handleSubmit}>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
@@ -46,15 +49,15 @@ const Signup = () => {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button disabled={loading}className="w-100" type="submit">Sign Up</Button>
+            <Button disabled={loading} className="w-100" type="submit">
+              Sign Up
+            </Button>
           </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? Log in
+        Already have an account? <Link to="/login">Log In</Link>
       </div>
     </>
-  );
+  )
 }
-
-export default Signup;
