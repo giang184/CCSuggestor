f2d5ce679f44e1b31cc1f1e2fc78e1150249dfe4
Andrew Giang <giang184@gmail.com>
2021-10-13 14:37:18 -0700

    change one to either

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
       <h1>How Do I Use CCSuggestor?</h1>
       <ul>
         <li><a href="/signup">Sign up</a> for an account. Or <a href="/signup">login</a> if you already have one.</li>
-        <li><a href="/create">Add cards</a> to your wallet using one of <strong>two options</strong>:
+        <li><a href="/create">Add cards</a> to your wallet using either of <strong>two options</strong>:
           <ol>
             <li>Manually add a custom card by providing a card name and selecting a card image. Then add the card's reward categories. This will populate in a fieldset that will allow you to set the category's cash back percentage.<br/><br/><img src={customAdd} alt="add card template" style={{width:'323px',height:'183px'}}/><br/><br/></li>
             <li>Click to select cards from a selected list. <br/><br/><img src={templateAdd} alt="add card custom" style={{width:'324px',height:'165px'}}/><br/><br/></li>
